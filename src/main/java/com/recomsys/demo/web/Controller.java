108336676c4fcc7912ee100169ea585fcbfcdfaf
JiangShiwei <shenmuyuyue@163.com>
2019-06-30 14:20:01 +0800

    Fix Controller

--- src/main/java/com/recomsys/demo/web/Controller.java
+++ src/main/java/com/recomsys/demo/web/Controller.java
@@ -3,16 +3,14 @@ package com.recomsys.demo.web;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class Controller {
-    @RestController
-    public class HelloController {
 
-        @RequestMapping("/hello")
-        /*
-         * RequestMapping作用 ： 提供路由信息，负责URL到Controller中的具体函数的映射
-         */
-        public String hello() {
-            return "Hello World";
-        }
+    /*
+     * RequestMapping作用 ： 提供路由信息，负责URL到Controller中的具体函数的映射
+     */
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello World";
     }
 }
